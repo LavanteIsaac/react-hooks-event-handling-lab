37beac3e92b863b95a023fd1c9d86a33e7ae95ad
Lavante Cotton <lavanteisaac@gmail.com>
2024-02-05 10:55:46 -0500

    inputting event handlers

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleChange = (event) => {
+        console.log('Entering password...', event.target.value)
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} />
+        </div>
     )
 }
 
